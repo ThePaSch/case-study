40181c9186ae93f2d9d40549ea80f11b513776e3
ThePaSch <contact@pascalschuster.de>
2025-08-26 14:49:40 +0200

    strict ts, eslint config, prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,33 @@
 // @ts-check
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
+  { 
+    rules: {
+        "no-self-compare": "error",
+        "no-useless-assignment": "error",
+        "dot-notation": "error",
+        "max-classes-per-file": ["error", 1],
+        "no-empty-function": "error",
+        "no-eq-null": "error",
+        "no-lonely-if": "error",
+        "no-magic-numbers": ["error", { "ignoreArrayIndexes": true, "ignore": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] }],
+        "no-multi-assign": "error",
+        "no-negated-condition": "error",
+        "no-nested-ternary": "error",
+        "no-sequences": "error",
+        "no-undefined": "error",
+        "no-unneeded-ternary": "error",
+        "no-useless-return": "error",
+        "no-useless-constructor": "error",
+        "no-var": "error",
+        "prefer-const": "error",
+        "require-await": "error",
+        "yoda": "error",
+        "@typescript-eslint/consistent-type-imports": "error"
+    }
+  },
+  eslintPluginPrettierRecommended
 )
